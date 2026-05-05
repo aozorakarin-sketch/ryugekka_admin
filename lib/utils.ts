cb6777f3a454707f746512446234597cb8dca3f1
aozorakarin <aozora.karin@gmail.com>
2026-05-05 18:49:04 +0900

    時間表示

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,15 +1,7 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
-
-
-
 export const formatJST = (s: string) => {
   if (!s) return "-"
-  const d = new Date(s)
+  const utc = s.endsWith('Z') ? s : s + 'Z'
+  const d = new Date(utc)
   const jst = new Date(d.getTime() + 9 * 60 * 60 * 1000)
   return `${jst.getUTCFullYear()}/${jst.getUTCMonth() + 1}/${jst.getUTCDate()} ${jst.getUTCHours().toString().padStart(2, "0")}:${jst.getUTCMinutes().toString().padStart(2, "0")}:${jst.getUTCSeconds().toString().padStart(2, "0")}`
 }
